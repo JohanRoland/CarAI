834246cb4c05d60dbd74646d45d545bc5d4bcf54
Knarkapan <johanroland@live.se>
2016-01-22 12:52:05 +0100

    Some OpenCV failure

---
+++ CarAI/CarAI/faceRecognition.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef faceRecognition_H
+#define faceRecognition_H
+
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <iostream>
+#include <stdio.h>
+
+
+class faceRecognition
+{
+public:	
+	faceRecognition();
+	int findFace();
+private:
+	void detectAndDisplay(cv::Mat frame);
+};
+
+
+#endif faceRecognition_H
