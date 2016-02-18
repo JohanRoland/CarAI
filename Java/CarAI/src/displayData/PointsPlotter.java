762da32ef536fcf0a73511f158700a15c07c2795
axhav <william_axhav@hotmail.com>
2016-02-18 11:01:59 +0100

    Added points plotter prt2

--- Java/CarAI/src/displayData/PointsPlotter.java
+++ Java/CarAI/src/displayData/PointsPlotter.java
@@ -2,14 +2,21 @@ package displayData;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 import interfaces.DatabaseLocation;
 import serverConnection.ServerConnection;
+import utils.Tuple;
+import utils.Utils;
 
 public class PointsPlotter extends JFrame {
 
@@ -20,12 +27,22 @@ public class PointsPlotter extends JFrame {
 	{
 		sc = ServerConnection.getInstance();
 		
-		
+		initUI();
 	}
 	
 	private void initUI()
 	{
 		final Surface surface = new Surface();
+		add(surface);
+		
+		addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                Timer timer = surface.getTimer();
+                timer.stop();
+            }
+        });
+		
 		setTitle("Plotter");
 		setSize(800,600);
 		setLocationRelativeTo(null);
@@ -35,21 +52,49 @@ public class PointsPlotter extends JFrame {
 	
 	
 	
-	class Surface extends JPanel 
+	class Surface extends JPanel implements ActionListener
 	{
+		private final int DELAY = 150;
+	    private Timer timer;
 		
 		public Surface()
 		{
-			initUI();
+			initTimer();
 		}
 		
+		private void initTimer() {
+
+	        timer = new Timer(DELAY, this);
+	        timer.start();
+	    }
+	    
+	    public Timer getTimer() {
+	        
+	        return timer;
+	    }
+		
+		 @Override
+	    public void paintComponent(Graphics g) {
+
+	        super.paintComponent(g);
+	        doDrawing(g);
+	    }
+		@Override
+	    public void actionPerformed(ActionEvent e) {
+	        repaint();
+	    }
+
+	    
 		private void doDrawing(Graphics g)
 		{
 			Graphics2D g2d = (Graphics2D) g;
 			try {
+				
 				ArrayList<DatabaseLocation> points = sc.getPosClass(0);
-				double scalingFacX = this.getWidth()-20 / getMaximum().x - getMinimum().x;
-				double scalingFacY = this.getHeight()-20 / getMaximum().y - getMinimum().y;
+				
+				Tuple<Tuple<Double,Double>,Tuple<Double,Double>> minMax =  Utils.getGPSPlotFrame(points);
+				double scalingFacX = this.getWidth()-20 / minMax.snd().fst() - minMax.fst().fst();
+				double scalingFacY = this.getHeight()-20 / minMax.snd().snd() - minMax.fst().snd();
 				double scalingFac = Math.min(scalingFacX, scalingFacY);
 				for(DatabaseLocation l: points)
 				{
