01a1f0b9e90fee617da1d6ef6bbeb08c64ca83ff
axhav <william_axhav@hotmail.com>
2016-05-23 17:20:11 +0200

    now elki is more linux friendly

--- Java/CarAI/src/mashinelearning/ELKIController.java
+++ Java/CarAI/src/mashinelearning/ELKIController.java
@@ -11,7 +11,7 @@ public class ELKIController {
 		
 		String currentPath = f.getAbsolutePath().substring(0, f.getAbsolutePath().length()-2);
     	
-		File file1 = new File(currentPath+"\\ELKIClusters");
+		File file1 = new File(currentPath+File.separator+"ELKIClusters");
     	
     	for(File file: file1.listFiles()) file.delete();
 		
@@ -19,7 +19,7 @@ public class ELKIController {
 		int minp = 3;
 		Runtime rt = Runtime.getRuntime();
 		//-dbc.in "D:\\Programming projects\\NIB\\CarAI\\Java\\CarAI\\testELKI.txt" -dbc.parser NumberVectorLabelParser -db.index tree.spatial.rstarvariants.rstar.RStarTreeFactory -spatial.bulkstrategy SortTileRecursiveBulkSplit -algorithm clustering.DBSCAN -algorithm.distancefunction geo.LatLngDistanceFunction -geo.model WGS84SpheroidEarthModel -dbscan.epsilon 50.0 -dbscan.minpts 10 -resulthandler ResultWriter -out "D:\\Programming projects\\NIB\\CarAI\\Java\\CarAI\\ELKIClusters"
-		String[] commands = {"java" ,"-jar" , "elki-bundle-0.7.1.jar","KDDCLIApplication", "-dbc.in","\"" +currentPath+ "\\testELKI.txt\"","-dbc.parser","NumberVectorLabelParser","-db.index","tree.spatial.rstarvariants.rstar.RStarTreeFactory","-spatial.bulkstrategy","SortTileRecursiveBulkSplit","-algorithm","clustering.DBSCAN","-algorithm.distancefunction","geo.LatLngDistanceFunction","-geo.model","WGS84SpheroidEarthModel","-dbscan.epsilon",""+eps,"-dbscan.minpts",""+minp,"-resulthandler","ResultWriter","-out","\""+currentPath + "\\ELKIClusters\""};
+		String[] commands = {"java" ,"-jar" , "elki-bundle-0.7.1.jar","KDDCLIApplication", "-dbc.in","\"" +currentPath+ File.separator+"testELKI.txt\"","-dbc.parser","NumberVectorLabelParser","-db.index","tree.spatial.rstarvariants.rstar.RStarTreeFactory","-spatial.bulkstrategy","SortTileRecursiveBulkSplit","-algorithm","clustering.DBSCAN","-algorithm.distancefunction","geo.LatLngDistanceFunction","-geo.model","WGS84SpheroidEarthModel","-dbscan.epsilon",""+eps,"-dbscan.minpts",""+minp,"-resulthandler","ResultWriter","-out","\""+currentPath + File.separator+"ELKIClusters\""};
 		String[] com2 = {"python","convert.py","coords.csv"};
 		
 		try {
