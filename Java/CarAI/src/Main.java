4290a528129db625080df28151b9fbf280c65bb8
axhav <william_axhav@hotmail.com>
2016-05-31 23:18:17 +0200

    changes that has been forgotten to be commited

--- Java/CarAI/src/Main.java
+++ Java/CarAI/src/Main.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintStream;
@@ -16,6 +17,7 @@ import java.util.Date;
 import java.util.Random;
 import java.util.stream.Stream;
 
+import org.encog.util.simple.EncogUtility;
 
 import car.CarInterface;
 import facerecognition.FaceMQTT;
@@ -222,48 +224,63 @@ public class Main
     		else if(args[0].equals("8"))
     		{
     		 	NNData n = new NNData();
-    		 	//n.parseKML("D:\\Programming projects\\NIB\\CarAI\\Java\\CarAI\\Platshistorik.kml", 0);
+    		 	n.parseKML("D:\\Programming projects\\NIB\\CarAI\\Java\\CarAI\\Platshistorik.kml", 0);
 
     		 	//n.parseKML("D:\\Programming projects\\NIB\\CarAI\\Java\\CarAI\\OlofLoc.kml", 0);
     		 	
-    		 	//n.coordCullByBox(57, 11, 2, 8);
+    		 	n.coordCullByBox(57, 11, 2, 8);
     		 	File f = new File(".");
 				String pathToProj = f.getAbsolutePath().substring(0, f.getAbsolutePath().length()-2);
 				
 				
-    		 	File data = new File("data");
-    		 	for(File fs : data.listFiles())
-    		 	{
-    		 		System.out.println("User: "+ fs.getName());
-    		 		n.parsGeoEntry(fs.getAbsolutePath());
-	    		 	System.out.println("Amount of Entries: " +n.getQuerry().size());
+    		 	//File data = new File("data");
+    		 	//for(File fs : data.listFiles())
+    		 	//{
+    		 		//System.out.println("User: "+ fs.getName());
+    		 		//n.parsGeoEntry(fs.getAbsolutePath());
+	    		 	
+					System.out.println("Amount of Entries: " +n.getQuerry().size());
+	    		 	int size1 = n.getQuerry().size();
 	    		 	if(n.getQuerry().size() != 0)
 	    		 	{
 	    		 		double dist1 = 0;
-		    		 	for(DatabaseLocation d : n.getQuerry())
+		    		 	/*for(DatabaseLocation d : n.getQuerry())
 		    		 	{
 		    		 		dist1 += Utils.distDB(d);
-		    		 	}
+		    		 	}*/
 		    		 	System.out.println("Dist before Distance culling: "+ dist1);
 		    		 	
-		    		 	n.coordCullBySpeed(25);;
+		    		 	n.coordCullBySpeed(25);
+		    		 	//n.coordCullByDist();
 		    		 	System.out.println("Amount of Entries after dist cull: " +n.getQuerry().size());
+		    		 	int size2 = n.getQuerry().size();
 		    		 	double dist2 = 0;
-		    		 	for(DatabaseLocation d : n.getQuerry())
+		    		 	/*for(DatabaseLocation d : n.getQuerry())
 		    		 	{
 		    		 		dist2 += Utils.distDB(d);
-		    		 	}
+		    		 	}*/
 		    		 	System.out.println("Dist after Distance culling: "+ dist2);
 		    		 	
 		    		 	Date date = new Date();
 		    		 	String tempName = "ELKIClusters" + date.getTime();
 		    		 	n.exportAsCoordsToCSV(pathToProj+File.separator+"coords.csv");
-		    			ELKIController.runElki(tempName);
+		    			boolean er = ELKIController.runElki(tempName);
 		    			ArrayList<ArrayList<DatabaseLocation>> clusters = n.importFromElkiClustering(tempName+File.separator);
 		    			System.out.println("Amount of Clusters " + (clusters.size()-1));
 		    		 	
-	    		 	}
+	    		 	
+		    		 	/*try {
+		    				BufferedWriter bw = new BufferedWriter(new FileWriter("resultofNoclustering.txt", true));
+		    				bw.write(fs.getName()+ "\t" + size1 + "\t" + dist1 +"\t"+  size2+"\t" + dist2 +"\t"+ (clusters.size()-1)+"\t" + er +"\n");
+		    				bw.close();
+		    			} catch (IOException e) {
+		    				// TODO Auto-generated catch block
+		    				e.printStackTrace();
+		    			}*/
+	    		 	//}
 	    		 }
+    		 	
+    		 	
     		}
     		else if(args[0].equals("9"))
     		{
