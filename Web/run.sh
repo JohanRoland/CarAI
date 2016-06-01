75da680906faa2c81d3eaa425aa6deb46da9e5db
Amee <olivercarlsson89@gmail.com>
2016-06-01 17:36:49 +0200

    update to the GUI

--- Web/run.sh
+++ Web/run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 CWD=$(pwd)
 input=`cat $1`
-echo $file
 cd ../Java/CarAI/
 echo "$input" | java -Djava.library.path=/home/amee/lib/ -jar target/CarAI-0.0.1-SNAPSHOT-jar-with-dependencies.jar "9"
 cd $CWD
